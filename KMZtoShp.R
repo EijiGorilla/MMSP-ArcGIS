6e59aaae8c9497b8c6b44b2f72307504725afded
松﨑 栄治 <oc3512@oriconsul.co.jp>
2019-02-15 14:05:56 +0800

    modified

--- KMZtoShp.R
+++ KMZtoShp.R
@@ -1,13 +1,16 @@
 # Convert KMZ file to shapfile in coordinate system for MMSP
 library(sf)
 
-file="C:/Users/oc3512/Documents/ArcGIS/Projects/MMSP/MainLines.kmz"
+# Choose kmz file
+file=choose.files() # example: C:/....kmz
+
+# Name the file:
+fileName="mmsp"
 
 # Unzip and convert kmz to kml
 unzip(file,junkpaths = TRUE) #creates "doc.kml"
 
 # read and convert Projection:
-data=st_read(file.path(getwd(),"doc.kml"))
 data=readOGR(file.path(getwd(),"doc.kml"))
 data_wgs84=spTransform(data,CRS=CRS("+init=epsg:32651"))
 
@@ -15,4 +18,5 @@ data_wgs84=spTransform(data,CRS=CRS("+init=epsg:32651"))
 proj4string(data_wgs84)
 
 # Save it in shpefile
-writeOGR(data,getwd(),"TESTT",driver="ESRI Shapefile")
+writeOGR(data,getwd(),fileName,driver="ESRI Shapefile")
+

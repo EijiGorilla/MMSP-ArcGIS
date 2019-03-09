f2a508d7e55a0b20243fb61f88e7c42891d24446
松﨑 栄治 <oc3512@oriconsul.co.jp>
2019-03-09 11:27:57 +0800

    delete

--- Reprojection.py
+++ Reprojection.py
@@ -8,20 +8,19 @@ How to Reproject vectors
 
 import arcpy
 import ListNamesFeatureClass
-import os
 
+# Set the environment
 arcpy.env.workspace="C:/Users/oc3512/Documents/ArcGIS/Projects/Python_practice/Lesson2"
 inFolder="C:/Users/oc3512/Documents/ArcGIS/Projects/Python_practice/Lesson2"
 arcpy.env.overwriteOutput=True
 
-
+# Read feature class
 featureList=arcpy.ListFeatureClasses()
-
 for feature in featureList:
     print("{0}".format(feature))
 
+# Define the target CRS
 targetCS=arcpy.Describe(featureList[1]).spatialReference
-
 
 
 for feature in featureList:
@@ -30,4 +29,30 @@ for feature in featureList:
         newName=feature.replace(".shp","_Projected.shp")
         arcpy.Project_management(feature,newName,targetCS)
 
+
+##########
+import arcpy
+import ListNamesFeatureClass
+import os
+
+arcpy.env.overwriteOutput=True
+
+out_Location="C:/Users/oc3512/Documents/ArcGIS/Projects/Python_practice"
+inFile="C:/Users/oc3512/Documents/ArcGIS/Projects/Python_practice/Stations.kmz"
+
+baseName=os.path.splitext(os.path.basename(inFile))[0] # returns
+arcpy.KMLToLayer_conversion(inFile,out_Location) # conver to a file geodatabase
+
+fcDataSets=ListNamesFeatureClass.listFeatureClassDatasetNames('C:/Users/oc3512/Documents/ArcGIS/Projects/Python_practice/Stations.gdb')
+fcFeatureList=ListNamesFeatureClass.listFeatureClassNames(fcDataSets)
+
+currentCRS=arcpy.Describe(fcFeatureList).spatialReference.Name
+
+targetCRS1=arcpy.SpatialReference("WGS 1984 UTM Zone 51N")
+targetCRS2=arcpy.SpatialReference("PRS 1992 Philippines Zone III")
+
+arcpy.CopyFeatures_management(fcFeatureList,"TEST123.shp")
+
+fc_targetCRS1=arcpy.Project_management("TEST123.shp","TEST123_32651.shp",targetCRS1)
+fc_targetCRS2=arcpy.Project_management("TEST123_32651.shp","TEST123_3123.shp",targetCRS2)
 

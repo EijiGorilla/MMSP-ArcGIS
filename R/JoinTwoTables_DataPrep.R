e2dec84a4d3c3314f763ae98d9e8b0dd366e8997
EijiGorilla <emasuz@hotmail.com>
2020-07-11 12:30:53 +0800

    Update JoinTwoTables_DataPrep.R

--- R/JoinTwoTables_DataPrep.R
+++ R/JoinTwoTables_DataPrep.R
@@ -14,11 +14,26 @@ y=read.xlsx(b)
 head(x)
 head(y)
 
-xy=left_join(x,y,by=c("Station", "CN"))
+#  table[,i]=as.Date(table[,i],format="%m/%d/%Y")
+#  table[,i]=as.POSIXct(table[,i],format="%m/%d/%y %H:%M:%S")
+# use the following date conversion if necessary
+y$ActualDateofInitialSubmissionforLegalPass = as.Date(y$ActualDateofInitialSubmissionforLegalPass, origin = "1899-12-30")
+y$ActualDateofInitialSubmissionforLegalPass = as.Date(y$ActualDateofInitialSubmissionforLegalPass, format="%m/%d/%y %H:%M:%S")
 
-xy=left_join(x,y,by="Id")
-        
-write.xlsx(xy,"C:/Users/oc3512/OneDrive - Oriental Consultants Global JV/Desktop/Envi/Time slice/v6/delete.xlsx",row.names=FALSE)
+y$ActualDateofClearedLegalPass = as.Date(y$ActualDateofClearedLegalPass, origin = "1899-12-30")
+y$ActualDateofClearedLegalPass = as.Date(y$ActualDateofClearedLegalPass, format="%m/%d/%y %H:%M:%S")
+
+
+
+
+xy=left_join(x,y,by="CN")
+xy=left_join(x,y,by=c("CN", "Station"))
+ 
+str(xy)
+       
+write.xlsx(xy,"C:/Users/oc3512/OneDrive - Oriental Consultants Global JV/Desktop/Envi/Time slice/v8/Joined.xlsx",row.names=FALSE)
+write.xlsx(xy,"C:/Users/oc3512/OneDrive - Oriental Consultants Global JV/Desktop/Envi/Parcellary Map/20200706/merged_with_MasterList.xlsx",row.names=FALSE)
+write.xlsx(xy,"C:/Users/oc3512/OneDrive - Oriental Consultants Global JV/Desktop/temp.xlsx",row.names=FALSE)
 
 
 ##########

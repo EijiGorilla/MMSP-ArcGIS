44b7510f44e0b5e1eb126e36ec54665ba7e4659a
EijiGorilla <emasuz@hotmail.com>
2022-03-07 12:52:37 +0800

    Create Copy_SDE_to_FGDB_using_WGS84.py

---
+++ Python/Copy_SDE_to_FGDB_using_WGS84.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  7 12:46:10 2022
+
+@author: oc3512
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  9 08:41:44 2021
+
+@author: emasu
+"""
+
+# This python script is to copy and convert source feature layer to PRS92 and truncate the target feature layer,
+# and finally append the source to the target.
+# This script is intendted to work with only one source (FGDB) and one taget feature layer (SDE).
+
+#
+import arcpy
+import re
+
+# To allow overwriting the outputs change the overwrite option to true.
+arcpy.env.overwriteOutput = True
+arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984 Web Mercator (auxiliary sphere)")
+arcpy.env.geographicTransformations = "PRS_1992_To_WGS_1984_1"
+
+
+# Script parameters
+workSpace = arcpy.GetParameterAsText(0)
+source_sde = arcpy.GetParameterAsText(1)
+target_fgdb = arcpy.GetParameterAsText(2)
+
+copied = "copied_layer"
+copyL = arcpy.CopyFeatures_management(source_sde, copied)
+arcpy.TruncateTable_management(target_fgdb)
+arcpy.Append_management(copyL, target_fgdb, schema_type = 'NO_TEST')
+
+# Delete
+arcpy.Delete_management(copyL)
+arcpy.AddMessage("Delete copied layer is Success")

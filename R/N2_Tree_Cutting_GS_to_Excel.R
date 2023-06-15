ac98c6151edfa49d8d46bf9da42615d76a623218
EijiGorilla <emasuz@hotmail.com>
2023-06-15 08:51:49 +0800

    Update N2_Tree_Cutting_GS_to_Excel.R

--- R/N2_Tree_Cutting_GS_to_Excel.R
+++ R/N2_Tree_Cutting_GS_to_Excel.R
@@ -3,11 +3,8 @@
 
 
 #############################
-new_date = "2022-08-01"
-
-
-
-
+old_date = "2022-03-13"
+new_date = "2022-04-12"
 ###############################
 
 
@@ -41,70 +38,72 @@ library(fs)
 ## Authorize (Choose 'matsuzakieiji0@gmail.com'. OCG gmail may not work)
 gs4_auth(email="matsuzaki-ei@ocglobal.jp")
 
+### N2:----
 # Choose working directory
 path = path_home()
-
-
 wd = file.path(path,"Dropbox/01-Railway/02-NSCR-Ex/01-N2/02-Pre-Construction/01-Environment/02-Tree Cutting")
 
 setwd(wd)
 
 ## Define URL where data is stored and updated
 url = "https://docs.google.com/spreadsheets/d/1LVcMTPahKD9-p_sTBL788qiUbFGTRc3jVgm_nlN-NQU/edit#gid=1298799693"
-
 MLTable = file.path(wd,"Trees_masterlist.xlsx")
-
 basename = basename(MLTable)
 
 y = read.xlsx(MLTable)
 
-# Read and write as CSV and xlsx
-v = range_read(url, sheet = 1)
-v = data.frame(v)
-
-
 # Create backup file of original masterlist
 # 
-
-dir = file.path(wd,"Dropbox/01-Railway/02-NSCR-Ex/01-N2/02-Pre-Construction/01-Environment/02-Tree Cutting/old")
+dir = file.path(wd,"old")
 
 y$updated = as.Date(y$updated, origin = "1899-12-30")
 y$updated = as.Date(y$updated, format="%m/%d/%y %H:%M:%S")
-
-
 old_date = gsub("-","",unique(y$updated))
 backup = paste(old_date,"_",basename,sep="") 
 
 write.xlsx(x,file.path(dir,backup),row.names=FALSE)
 
+# Read and write as CSV and xlsx
+v = range_read(url, sheet = 1)
+v = data.frame(v)
+
 ## Rename variables
-v2 = v
-v2$updated = new_date
-
-v2$updated = as.Date(v2$updated, origin = "1899-12-30")
-v2$updated = as.Date(v2$updated, format="%m/%d/%v2 %H:%M:%S")
-
-head(v2)
-str(v2)
-
-
+x = v
+x$updated = new_date
+x$updated = as.Date(x$updated, origin = "1899-12-30")
+x$updated = as.Date(x$updated, format="%m/%d/%x %H:%M:%S")
 
 # Rename colnames
-colnames(v2)=c("TreeNo","Province","CP","CommonName","ScientificName",
+colnames(x)=c("TreeNo","Province","CP","CommonName","ScientificName",
                "DBH","MH","TH","Volume","Latitude","Longitude",
                "PNR","Status","Conservation","Compensation","updated")
-for(i in 1:5) v2$CP = gsub(i,paste("N-0",i,sep=""),v2$CP)
+for(i in 1:5) x$CP = gsub(i,paste("N-0",i,sep=""),x$CP)
 
-write.xlsx(v2,file.path(wd,basename))
+write.xlsx(x,file.path(wd,basename))
 
 
 
-###############
-head(v2)
-x=v2
-cut = length(which(x$Status==1))
-total = nrow(x)
-cut/total
+#########################################
+### SC:----
+#a = choose.dir()
+path = path_home()
+a = file.path(path, "Dropbox\\01-Railway\\02-NSCR-Ex\\02-SC\\02-Pre-Construction\\01-Environment\\03-Tree\\01-Masterlist")
 
-head(x)
-length(x$TreeNo)
+fileNames = list.files(a)
+names = fileNames[str_detect(fileNames, "^SC_Main.*.xlsx$|^SC_PNR.*xlsx$")]
+x = read.xlsx(file.path(a,names[1]))
+x1 = read.xlsx(file.path(a,names[2]))
+
+colnames(x)=c("CP","Barangay","District","Municipality","TreeNo",
+               "LocalName","FamilyName","ScientificName",
+               "DBH","MH","TH","Volume","Longitude","Latitude",
+               "Conservation","StemQuality","Planted","LotNo","PNR_ROW","Status")
+
+colnames(x1)=c("CP","Barangay","District","Municipality","TreeNo",
+              "LocalName","FamilyName","ScientificName",
+              "DBH","MH","TH","Volume","Longitude","Latitude",
+              "Conservation","StemQuality","Planted","LotNo","PNR_ROW","Status")
+
+xx = rbind(x,x1)
+
+write.xlsx(xx, "SC_Tree_merged.xlsx")
